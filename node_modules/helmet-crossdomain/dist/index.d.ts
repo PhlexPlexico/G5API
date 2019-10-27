0f14eac42b2d25108e8c813d2969dda12cb04983
Phlex Plexico <flexing@phlexplexi.co>
2019-10-27 16:14:36 -0600

    Removing Mistakenly tracked files.

--- node_modules/helmet-crossdomain/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { IncomingMessage, ServerResponse } from 'http';
-interface CrossDomainOptions {
-    permittedPolicies?: string;
-}
-declare const _default: (options?: CrossDomainOptions) => (_req: IncomingMessage, res: ServerResponse, next: () => void) => void;
-export = _default;
