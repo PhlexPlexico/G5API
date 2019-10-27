4ffa4fd9d5c4289ca7fb8d257a5e48ee01808565
Phlex Plexico <flexing@phlexplexi.co>
2019-10-27 16:14:36 -0600

    Removing Mistakenly tracked files.

--- node_modules/dont-sniff-mimetype/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = function nosniff() {
-    return function nosniff(_req, res, next) {
-        res.setHeader('X-Content-Type-Options', 'nosniff');
-        next();
-    };
-};
--- node_modules/helmet-csp/dist/lib/is-function.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-module.exports = function isFunction(value) {
-    return value instanceof Function;
-};
