1cc7c62dd88e671979e62d5671c105742da90742
Evan <etc114@mail.usask.ca>
2019-10-26 18:51:27 -0600

    Formatting of documents with Prettier.

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,16 @@
  * @requires express
  * @requires db
  */
-var express = require('express');
+var express = require("express");
 /** Express module
  * @const
  */
 const router = express.Router();
 /** Database module.
  * @const
- */ 
-const db = require('../db');
+ */
+
+const db = require("../db");
 
 /** GET - Route serving to get all users.
  * @name router.get('/')
@@ -21,14 +22,14 @@ const db = require('../db');
  * @param {string} path - Express path
  * @param {callback} middleware - Express middleware.
  */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   var sql = "SELECT * FROM user";
   db.query(sql, function(err, rows) {
     if (err) {
-      res.status(500).send({ error: 'Something failed!' + err });
+      res.status(500).send({ error: "Something failed!" + err });
     }
     res.json(rows);
-  })
+  });
 });
 
 /** GET - Route serving to get one user by database or steam id.
@@ -40,15 +41,15 @@ router.get('/', function(req, res, next) {
  * @param {number} request.param.userid - The database or steam ID of the user.
  * @param {callback} middleware - Express middleware.
  */
-router.get('/:userid', function(req, res, next) {
-  userOrSteamID = req.param('userid');
+router.get("/:userid", function(req, res, next) {
+  userOrSteamID = req.param("userid");
   var sql = "SELECT * FROM user where id = ? OR steam_id = ?";
   db.query(sql, [userOrSteamID, userOrSteamID], function(err, rows) {
     if (err) {
-      res.status(500).send({ error: 'Something failed!' + err });
+      res.status(500).send({ error: "Something failed!" + err });
     }
     res.json(rows);
-  })
+  });
 });
 
 /** POST - Route serving to insert a user into the database.
@@ -60,18 +61,22 @@ router.get('/:userid', function(req, res, next) {
  * @param {number} req.body.admin - Integer determining if a user is an admin of the system. Either 1 or 0.
  * @param {number} req.body.super_admin - Integer determining if a user is a super admin of the system. Either 1 or 0.
  */
-router.post('/create', function(req, res, next) {
+router.post("/create", function(req, res, next) {
   var steamId = req.body.steam_id;
   var steamName = req.body.name;
   var isAdmin = req.body.admin;
   var isSuperAdmin = req.body.super_admin;
-  var sql = "INSERT INTO user (steam_id, name, admin, super_admin) VALUES (?,?,?,?)";
-  db.query(sql, [steamId, steamName, isAdmin, isSuperAdmin], function(err, result) {
+  var sql =
+    "INSERT INTO user (steam_id, name, admin, super_admin) VALUES (?,?,?,?)";
+  db.query(sql, [steamId, steamName, isAdmin, isSuperAdmin], function(
+    err,
+    result
+  ) {
     if (err) {
-      res.status(500).send({ error: 'Something failed!' + err });
+      res.status(500).send({ error: "Something failed!" + err });
     }
-    res.json({"message": "User created successfully"});
-  })
+    res.json({ message: "User created successfully" });
+  });
 });
 
 /** PUT - Route serving to update a user admin privilege in the application.
@@ -82,18 +87,17 @@ router.post('/create', function(req, res, next) {
  * @param {number} req.body.admin - Integer determining if a user is an admin of the system. Either 1 or 0.
  * @param {number} req.body.super_admin - Integer determining if a user is a super admin of the system. Either 1 or 0.
  */
-router.put('/update', function(req, res, next) {
+router.put("/update", function(req, res, next) {
   var steamId = req.body.steam_id;
   var isAdmin = req.body.admin || 0;
   var isSuperAdmin = req.body.super_admin || 0;
   var sql = "UPDATE user SET admin = ?, super_admin = ? WHERE steam_id = ?";
   db.query(sql, [isAdmin, isSuperAdmin, steamId], function(err, result) {
     if (err) {
-      res.status(500).send({ error: 'Something failed!' + err });
+      res.status(500).send({ error: "Something failed!" + err });
     }
-    res.json({"message": "User created successfully"});
-  })
+    res.json({ message: "User created successfully" });
+  });
 });
-
 
 module.exports = router;
