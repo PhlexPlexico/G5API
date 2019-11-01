428bd542341effdc652c847ade57fc2366798577
Phlex Plexico <flexing@phlexplexi.co>
2019-10-31 18:59:20 -0600

    Promises implemented, update needed to be done.

--- db.js
+++ db.js
@@ -1,16 +1,40 @@
 var mysql = require('mysql');
 var config = require('config');
+const util = require( 'util' );
 
-var connection = mysql.createConnection({
+const dbCfg = {
   socketPath: config.get("Database.sockFile"),
   user: config.get("Database.username"),
   password: config.get("Database.password"),
   database: config.get("Database.db")
-});
+}
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log('connected!');
-});
+function makeDb( config ) {
+  const connection = mysql.createConnection( config );  return {
+    query( sql, args ) {
+      return util.promisify( connection.query )
+        .call( connection, sql, args );
+    },
+    close() {
+      return util.promisify( connection.end ).call( connection );
+    },
+    beginTransaction() {
+      return util.promisify( connection.beginTransaction )
+        .call( connection );
+    },
+    commit() {
+      return util.promisify( connection.commit )
+        .call( connection );
+    },
+    rollback() {
+      return util.promisify( connection.rollback )
+        .call( connection );
+    }
+  };
+}
 
-module.exports = connection;
+
+
+const conn = makeDb( dbCfg );
+
+module.exports = conn;
