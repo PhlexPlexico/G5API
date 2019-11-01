f5ce1227126fa97d8e196e0abd21b876df463047
Phlex Plexico <flexing@phlexplexi.co>
2019-10-31 18:59:20 -0600

    Promises implemented, update needed to be done.

--- routes/teams.js
+++ routes/teams.js
@@ -3,7 +3,7 @@
  * @requires express
  * @requires db
  */
-var express = require("express");
+let express = require("express");
 /** Express module
  * @const
  */
@@ -18,24 +18,23 @@ const db = require("../db");
  * @function
  * @memberof module:routes/teams
  */
-router.get("/", function(req, res, next) {
-  var sql =
-    "SELECT t.id, t.user_id, t.name, t.flag, t.logo, t.tag, t.public_team, " +
+router.get("/", async (req, res, next) => {
+  let sql =
+    "SELECT t.id, t.name, t.flag, t.logo, t.tag, t.public_team, " +
     "CONCAT('{', GROUP_CONCAT( DISTINCT CONCAT('\"',ta.auth, '\"', ': \"', ta.name, '\"')  SEPARATOR ', '), '}') as auth_name " +
     "FROM team t JOIN team_auth_names ta " +
     "ON t.id = ta.team_id " +
-    "GROUP BY t.id, t.user_id, t.name, t.flag, t.logo, t.tag, t.public_team";
-  db.query(sql, function(err, rows) {
-    if (err) {
-      res.send({ error: "Something failed!" + err });
+    "GROUP BY t.name, t.flag, t.logo, t.tag, t.public_team";
+    try {
+      const allTeams = await db.query( sql );
+      // do something with someRows and otherRows
+      allTeams.forEach((row) => {
+        row.auth_name = JSON.parse(row.auth_name);
+      });
+      res.json(allTeams);
+    } catch ( err ) {
+      res.json({message: err});
     }
-    // Need to perform some data manipulation for each row to change the arrayBuffers to meaningful strings/JSON\
-    rows.forEach(function(row) {
-      row.auth_name = JSON.parse(row.auth_name);
-    });
-
-    res.json(rows);
-  });
 });
 
 /** GET - Route serving to get all teams.
@@ -44,26 +43,22 @@ router.get("/", function(req, res, next) {
  * @memberof module:routes/teams
  * @param {int} teamid - The team ID you wish to examine.
  */
-router.get("/:teamid", function(req, res, next) {
+router.get("/:teamid", async (req, res, next) => {
   teamID = req.params.teamid;
-  var sql =
-    "SELECT t.id, t.user_id, t.name, t.flag, t.logo, t.tag, t.public_team, " +
+  let sql =
+    "SELECT t.id, t.name, t.flag, t.logo, t.tag, t.public_team, " +
     "CONCAT('{', GROUP_CONCAT( DISTINCT CONCAT('\"',ta.auth, '\"', ': \"', ta.name, '\"')  SEPARATOR ', '), '}') as auth_name " +
     "FROM team t JOIN team_auth_names ta " +
     "ON t.id = ta.team_id  " +
     "where t.id = ?";
-
-  db.query(sql, [teamID], function(err, rows) {
-    if (err) {
-      res.send({ error: "Something failed!" + err });
+    try {
+      const allTeams = await db.query( sql, teamID );
+      // do something with someRows and otherRows
+      allTeams[0].auth_name = JSON.parse(allTeams[0].auth_name)
+      res.json(allTeams);
+    } catch ( err ) {
+      res.status(500).json({message: err});
     }
-    rows[0].auth_name = JSON.parse(rows[0].auth_name);
-    console.log(
-      Object.values(rows[0].auth_name),
-      Object.keys(rows[0].auth_name)
-    );
-    res.json(rows);
-  });
 });
 
 /** POST - Route serving to insert a user into the database.
@@ -82,17 +77,16 @@ router.get("/:teamid", function(req, res, next) {
  * @see https://steamcommunity.com/sharedfiles/filedetails/?id=719079703
  */
 
-//TODO: Test the inserts, make sure lists are inserted as blobs.
-router.post("/create", function(req, res, next) {
-  var userID = req.body[0].user_id;
-  var teamName = req.body[0].name;
-  var flag = req.body[0].flag;
-  var logo = req.body[0].logo;
-  var auths = req.body[0].auth_name;
-  var tag = req.body[0].tag;
-  var public_team = req.body[0].public_team;
+router.post("/create", async (req, res, next) => {
+  let userID = req.body[0].user_id;
+  let teamName = req.body[0].name;
+  let flag = req.body[0].flag;
+  let logo = req.body[0].logo;
+  let auths = req.body[0].auth_name; // Sent into here as a list? Verify somehow?
+  let tag = req.body[0].tag;
+  let public_team = req.body[0].public_team;
   let teamID = null;
-  var newTeam = [
+  newTeam = [
     {
       user_id: userID,
       name: teamName,
@@ -102,68 +96,22 @@ router.post("/create", function(req, res, next) {
       public_team: public_team
     }
   ];
-  db.beginTransaction(err => {
-    var sql =
-      "INSERT INTO team (user_id, name, flag, logo, tag, public_team) VALUES ?";
-    db.query(
-      sql,
-      [
-        newTeam.map(item => [
-          item.user_id,
-          item.name,
-          item.flag,
-          item.logo,
-          item.tag,
-          item.public_team
-        ])
-      ],
-      (err, result) => {
-        if (err) {
-          db.rollback(() => {
-            console.log(err);
-          });
-          return res.send({ error: "Something failed!" + err });
-        }
-        teamID = result.insertId;
-        sql = "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?,?,?)";
-        console.log(
-          "Now keys and vals: " +
-            Object.keys(auths) +
-            " " +
-            Object.values(auths)
-        );
-        for(let key in auths){
-          db.query(
-          sql,
-          [teamID, key, auths[key]],
-          function(err, result) {
-            if (err) {
-              db.rollback(() => {
-                console.log(err);
-              });
-              throw err;
-            }
-          }
-        );
-        }
-        
-      }
-    );
+  let sql = "INSERT INTO team (user_id, name, flag, logo, tag, public_team) VALUES ?";
 
-    db.commit(err => {
-      if (err) {
-        db.rollback(() => {
-          console.log(err);
-        });
-        return res.send({ error: "Something failed!" + err });
+  try {
+    await withTransaction( db, async () => {
+      const insertTeam = await db.query( sql, [newTeam.map(item => [item.user_id, item.name, item.flag, item.logo, item.tag, item.public_team])] );
+      teamID = insertTeam.insertId;
+      sql = "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?, ?, ?)";
+      console.log("We made it past the first insert.")
+      for(let key in auths){
+        await db.query( sql, [teamID, key, auths[key]]);
       }
+      res.json({message: "Team successfully inserted with ID " + teamID});
     });
-    return res.json({
-      message: "Team created successfully."
-    });
-    // values.name
-    // Opting for KeyValue pairs to insert into the database. Can then do builk insert?
-  });
+  } catch ( err ) {
+    res.status(500).json({message: err});
+  }
 });
 
 //TODO: Finish update statement.
@@ -171,47 +119,55 @@ router.post("/create", function(req, res, next) {
  * @name /update
  * @function
  * @memberof module:routes/teams
- * @param {int} req.body[0].id - the Team id stored in the database..
+ * @param {int} req.body[0].id - the Team id stored in the database.
+ * @param {int} req.body[0].steam_id - The Steam ID of the user making the change.
  * @param {string} req.body[0].name - Steam ID of the user being created.
  * @param {string} req.body[0].flag - International Flag code by Steam.
  * @param {string} req.body[0].logo - Integer determining if a user is a super admin of the system. Either 1 or 0.
- * @param {list} req.body[0].auths - An object of type list containing all steam 64 auths on a team.
+ * @param {JSON} req.body[0].auth_name - An object of type list containing all steam 64 auths on a team.
  * @param {string} req.body[0].tag - A string with a shorthand tag for a team.
  * @param {number} req.body[0].public_team - Integer determining if a team is a publically usable team. Either 1 or 0.
- * @param {list} req.body[0].preferred_names - List containing a 1:1 relation to user auths.
  * @see https://steamcommunity.com/sharedfiles/filedetails/?id=719079703
  */
 router.put("/update", function(req, res, next) {
-  var columns = [];
-  var values = [];
-  var team_id = req.body[0].id;
-  var queryStr;
+  let teamID = req.body[0].id;
+  let teamName = req.body[0].name;
+  let teamFlag = req.body[0].flag;
+  let teamAuths = req.body[0].auth_name;
+  let teamTag = req.body[0].tag;
+  let publicTeam = req.body[0].public_team;
 
-  if (typeof req.body[0].name !== "undefined")
-    columns.push("name = " + req.body[0].name);
-
-  if (typeof req.body[0].flag !== "undefined")
-    columns.push("flag = " + req.body[0].flag);
-
-  if (typeof req.body[0].logo !== "undefined")
-    columns.push("logo = " + req.body[0].logo);
-
-  if (typeof req.body[0].tag !== "undefined")
-    columns.push("tag = " + req.body[0].tag);
-
-  if (typeof req.body[0].public_team !== "public_team")
-    columns.push("public_team = " + req.body[0].public_team);
-
-  var sql = "UPDATE team SET " + columns + " WHERE id=?";
-  db.query(sql, [team_id], function(err, result) {
-    if (err) {
-      res.send({ error: "Something failed!" + err });
-    }
-    //res.json({ message: "Team edited successfully" });
-  });
-  // TODO: Check if team was edited, and if anyone was removed or added.
+  let sql = "UPDATE team SET name = ?, flag = ?, logo = ?, tag = ?, public_team = ? WHERE id=? and user_id = ?";
+  try {
+    await withTransaction( db, async () => {
+      const insertTeam = await db.query( sql, [newTeam.map(item => [item.user_id, item.name, item.flag, item.logo, item.tag, item.public_team])] );
+      teamID = insertTeam.insertId;
+      sql = "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?, ?, ?)";
+      console.log("We made it past the first insert.")
+      for(let key in auths){
+        await db.query( sql, [teamID, key, auths[key]]);
+      }
+      res.json({message: "Team successfully inserted with ID " + teamID});
+    });
+  } catch ( err ) {
+    res.status(500).json({message: err});
+  }
 });
 
-//TODO: Various getters/setters are needed to be imported from Get5-Web. Please see https://github.com/PhlexPlexico/get5-web/blob/master/get5/models.py
+
+async function withTransaction( db, callback ) {
+  try {
+    await db.beginTransaction();
+    await callback();
+    await db.commit();
+  } catch ( err ) {
+    await db.rollback();
+    throw err;
+  } finally {
+    await db.close();
+  }
+}
+
+//TODO: letious getters/setters are needed to be imported from Get5-Web. Please see https://github.com/PhlexPlexico/get5-web/blob/master/get5/models.py
 
 module.exports = router;
