c324b40fb2afb839d7711ee24a1839cb4e89b314
Evan <etc114@mail.usask.ca>
2019-10-31 22:35:06 -0600

    Docs updated, teams now has basic implementation.

--- routes/teams.js
+++ routes/teams.js
@@ -25,19 +25,19 @@ router.get("/", async (req, res, next) => {
     "FROM team t JOIN team_auth_names ta " +
     "ON t.id = ta.team_id " +
     "GROUP BY t.name, t.flag, t.logo, t.tag, t.public_team";
-    try {
-      const allTeams = await db.query( sql );
-      // do something with someRows and otherRows
-      allTeams.forEach((row) => {
-        row.auth_name = JSON.parse(row.auth_name);
-      });
-      res.json(allTeams);
-    } catch ( err ) {
-      res.json({message: err});
-    }
+  try {
+    const allTeams = await db.query(sql);
+    // do something with someRows and otherRows
+    allTeams.forEach(row => {
+      row.auth_name = JSON.parse(row.auth_name);
+    });
+    res.json(allTeams);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-/** GET - Route serving to get all teams.
+/** GET - Route serving to get team with an ID.
  * @name /
  * @function
  * @memberof module:routes/teams
@@ -51,27 +51,25 @@ router.get("/:teamid", async (req, res, next) => {
     "FROM team t JOIN team_auth_names ta " +
     "ON t.id = ta.team_id  " +
     "where t.id = ?";
-    try {
-      const allTeams = await db.query( sql, teamID );
-      // do something with someRows and otherRows
-      allTeams[0].auth_name = JSON.parse(allTeams[0].auth_name)
-      res.json(allTeams);
-    } catch ( err ) {
-      res.status(500).json({message: err});
-    }
+  try {
+    const allTeams = await db.query(sql, teamID);
+    // do something with someRows and otherRows
+    allTeams[0].auth_name = JSON.parse(allTeams[0].auth_name);
+    res.json(allTeams);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-/** POST - Route serving to insert a user into the database.
+/** POST - Route serving to insert a team into the database.
  * @name /create
  * @function
  * @memberof module:routes/teams
- * @param {string} req.body[0].data- Crafted JSON Data sent as it is received via get.
- * @param {string} req.body[0].name - Name inputted by a user.
+ * @param {int} req.body[0].user_id - The ID of the user creating the team to claim ownership.
+ * @param {string} req.body[0].name - Team name inputted by a user.
  * @param {string} req.body[0].flag - International code for a flag.
  * @param {string} req.body[0].logo - A string representing the logo stored on the webserver.
  * @param {JSON} req.body[0].auth_name - A JSON KV pair containing the SteamID of a player as the key, and a value, or blank string value as the preferred name.
- * @param {JSON} req.body[0].preferred_names - A JSON array containing the Steam64 IDs for players. 
-                                            This must match the size of the auths if used. Use a blank value if a user does not wish to have a name.
  * @param {string} req.body[0].tag - A string with a shorthand tag for a team.
  * @param {number} req.body[0].public_team - Integer determining if a team is a publically usable team. Either 1 or 0.
  * @see https://steamcommunity.com/sharedfiles/filedetails/?id=719079703
@@ -96,35 +94,44 @@ router.post("/create", async (req, res, next) => {
       public_team: public_team
     }
   ];
-  let sql = "INSERT INTO team (user_id, name, flag, logo, tag, public_team) VALUES ?";
+  let sql =
+    "INSERT INTO team (user_id, name, flag, logo, tag, public_team) VALUES ?";
 
   try {
-    await withTransaction( db, async () => {
-      const insertTeam = await db.query( sql, [newTeam.map(item => [item.user_id, item.name, item.flag, item.logo, item.tag, item.public_team])] );
+    await withTransaction(db, async () => {
+      const insertTeam = await db.query(sql, [
+        newTeam.map(item => [
+          item.user_id,
+          item.name,
+          item.flag,
+          item.logo,
+          item.tag,
+          item.public_team
+        ])
+      ]);
       teamID = insertTeam.insertId;
-      sql = "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?, ?, ?)";
-      console.log("We made it past the first insert.")
-      for(let key in auths){
-        await db.query( sql, [teamID, key, auths[key]]);
+      sql =
+        "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?, ?, ?)";
+      console.log("We made it past the first insert.");
+      for (let key in auths) {
+        await db.query(sql, [teamID, key, auths[key]]);
       }
-      res.json({message: "Team successfully inserted with ID " + teamID});
+      res.json({ message: "Team successfully inserted with ID " + teamID });
     });
-  } catch ( err ) {
-    res.status(500).json({message: err});
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
 
-// TODO - Update documentation.
-/** PUT - Route serving to update a user admin privilege in the application. Submit through form to update the required data.
+/** PUT - Route serving to udpate a team into the database.
  * @name /update
  * @function
  * @memberof module:routes/teams
- * @param {int} req.body[0].id - the Team id stored in the database.
- * @param {int} req.body[0].steam_id - The Steam ID of the user making the change.
- * @param {string} req.body[0].name - Steam ID of the user being created.
- * @param {string} req.body[0].flag - International Flag code by Steam.
- * @param {string} req.body[0].logo - Integer determining if a user is a super admin of the system. Either 1 or 0.
- * @param {JSON} req.body[0].auth_name - An object of type list containing all steam 64 auths on a team.
+ * @param {int} req.body[0].user_id - The ID of the user creating the team to claim ownership.
+ * @param {string} req.body[0].name - Team name inputted by a user.
+ * @param {string} req.body[0].flag - International code for a flag.
+ * @param {string} req.body[0].logo - A string representing the logo stored on the webserver.
+ * @param {JSON} req.body[0].auth_name - A JSON KV pair containing the SteamID of a player as the key, and a value, or blank string value as the preferred name. If no preferred name, than an empty string accompnies the value.
  * @param {string} req.body[0].tag - A string with a shorthand tag for a team.
  * @param {number} req.body[0].public_team - Integer determining if a team is a publically usable team. Either 1 or 0.
  * @see https://steamcommunity.com/sharedfiles/filedetails/?id=719079703
@@ -147,32 +154,101 @@ router.put("/update", async (req, res, next) => {
       public_team: publicTeam
     }
   ];
-  let sql = "UPDATE team SET name = ?, flag = ?, logo = ?, tag = ?, public_team = ? WHERE id=? and user_id = ?";
+  let sql =
+    "UPDATE team SET name = ?, flag = ?, logo = ?, tag = ?, public_team = ? WHERE id=? and user_id = ?";
   try {
-    await withTransaction( db, async () => {
-      await db.query( sql, [newTeam.map(item => [item.name, item.flag, item.logo, item.tag, item.public_team, item.user_id])] );
-      sql = "UPDATE team_auth_names SET name = ? WHERE auth = ? AND team_id = ?";
-      for(let key in teamAuths){
-        await db.query( sql, [auths[key], key, teamID]);
+    await withTransaction(db, async () => {
+      await db.query(sql, [
+        newTeam.map(item => [
+          item.name,
+          item.flag,
+          item.logo,
+          item.tag,
+          item.public_team,
+          item.user_id
+        ])
+      ]);
+      sql =
+        "UPDATE team_auth_names SET name = ? WHERE auth = ? AND team_id = ?";
+      for (let key in teamAuths) {
+        await db.query(sql, [auths[key], key, teamID]);
       }
-      res.json({message: "Team successfully updated" });
+      res.json({ message: "Team successfully updated" });
     });
-  } catch ( err ) {
-    res.status(500).json({message: err});
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
 
-// TODO: DELETE STMT. Check if team has matches, return not allowed if they have matches in their name.
-router.delete('/delete', async (req,res,next) => {
-  res.status(500).json({message: "NOT IMPLEMENTED."});
+/** DELETE - Route serving to delete a team from the database. The team will only be deleted if their foreign key references have been removed, as we want to keep statistics of all teams over time.
+ * @name /delete/:team_id
+ * @function
+ * @memberof module:routes/teams
+ * @param {int} req.params.team_id - The ID of the team to be deleted.
+ */
+router.delete("/delete/:team_id", async (req, res, next) => {
+  let teamID = req.params.team_id;
+  try {
+    // First find any matches/mapstats/playerstats associated with the team.
+    let playerStatSql =
+      "SELECT COUNT(*) as RECORDS FROM player_stats WHERE team_id = ?";
+    let mapStatSql =
+      "SELECT COUNT(*) as RECORDS FROM map_stats WHERE winner = ?";
+    let matchSql =
+      "SELECT COUNT(*) as RECORDS FROM `match` WHERE team1_id = ? OR team2_id = ?";
+    const playerStatCount = await db.query(playerStatSql, teamID);
+    const mapStatCount = await db.query(mapStatSql, teamID);
+    const matchCount = await db.query(matchSql, [teamID, teamID]);
+    console.log(
+      "PlayerStat: " +
+        JSON.stringify(playerStatCount) +
+        "\nmapStat: " +
+        JSON.stringify(mapStatCount) +
+        "\nMatch: " +
+        JSON.stringify(matchCount)
+    );
+    if (
+      playerStatCount[0].RECORDS > 0 ||
+      mapStatCount[0].RECORDS > 0 ||
+      matchCount[0].RECORDS > 0
+    ) {
+      throw "Cannot delete team as it has more than one of the following true:\n" +
+        "Player Stat Records: " +
+        playerStatCount[0].RECORDS +
+        "\n" +
+        "Map Stat Records: " +
+        mapStatCount[0].RECORDS +
+        "\n" +
+        "Match Records: " +
+        matchCount[0].RECORDS;
+    }
+    // Otherwise, let's continue with delete. Start with auths.
+    await withTransaction(db, async () => {
+      let deleteTeamAuthSql = "DELETE FROM team_auth_names WHERE team_id = ?";
+      let deleteTeamsql = "DELETE FROM team WHERE id = ?";
+      await db.query(deleteTeamAuthSql, teamID);
+      await db.query(deleteTeamsql, teamID);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+  res.json({ message: "Team has been delete succesfully!" });
 });
 
-async function withTransaction( db, callback ) {
+/** Inner function - boilerplate transaction call.
+ * @name withTransaction
+ * @function
+ * @inner
+ * @memberof module:routes/teams
+ * @param {*} db - The database object.
+ * @param {*} callback - The callback function that is operated on, usually a db.query()
+ */
+async function withTransaction(db, callback) {
   try {
     await db.beginTransaction();
     await callback();
     await db.commit();
-  } catch ( err ) {
+  } catch (err) {
     await db.rollback();
     throw err;
   } finally {
@@ -180,6 +256,6 @@ async function withTransaction( db, callback ) {
   }
 }
 
-//TODO: letious getters/setters are needed to be imported from Get5-Web. Please see https://github.com/PhlexPlexico/get5-web/blob/master/get5/models.py
+//TODO: various getters/setters are needed to be imported from Get5-Web. Please see https://github.com/PhlexPlexico/get5-web/blob/master/get5/models.py
 
 module.exports = router;
