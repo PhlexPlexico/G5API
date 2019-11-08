ff71f3e88000a77621021137e35244421179dd86
Evan <etc114@mail.usask.ca>
2019-11-07 22:50:00 -0600

    More mapstat feature updates.

--- routes/legacy/api.js
+++ routes/legacy/api.js
@@ -38,8 +38,25 @@ const basicRateLimit = rateLimit({
     }
 });
 
+const updateMapRateLimit = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 1000,
+    message: "Too many requests from this IP. Please try again in an hour.",
+    keyGenerator: async (req) => {      
+      try{
+        const api_key = await db.query("SELECT api_key FROM `match` WHERE id = ?", req.params.match_id);
+        if(api_key[0].api_key.localeCompare(req.query.key))
+          return api_key[0].api_key;
+        else
+          return req.ip;
+      } catch (err){
+        return req.ip;
+      }
+    }
+});
+
 /** POST - API Call to finish a match.
- * @name /
+ * @name router.post('/:match_id/finish')
  * @function
  * @memberof module:routes/legacy/api
  * @param {string} req.query.key - The API key given from the game server to compare.
@@ -115,85 +132,128 @@ router.post("/:match_id/finish", basicRateLimit, async (req, res, next) => {
  * @name /
  * @function
  * @memberof module:routes/legacy/api
- * @param {int} teamid - The team ID you wish to examine.
+ * @param {int} req.params.match_id - The given match ID to start.
+ * @param {int} req.params.map_number - The given map number to start.
+ * @param {string} req.params.mapname - The given map name to update in the map stats object.
  */
-router.post("/:match_id/map/:map_number/start", async (req, res, next) => {
-  teamID = req.params.teamid;
-  let sql =
-    "SELECT t.id, t.name, t.flag, t.logo, t.tag, t.public_team, " +
-    "CONCAT('{', GROUP_CONCAT( DISTINCT CONCAT('\"',ta.auth, '\"', ': \"', ta.name, '\"')  SEPARATOR ', '), '}') as auth_name " +
-    "FROM team t JOIN team_auth_names ta " +
-    "ON t.id = ta.team_id  " +
-    "where t.id = ?";
+router.post("/:match_id/map/:map_number/start", basicRateLimit, async (req, res, next) => {
   try {
-    const allTeams = await db.query(sql, teamID);
-    // do something with someRows and otherRows
-    allTeams[0].auth_name = JSON.parse(allTeams[0].auth_name);
-    res.json(allTeams);
+    // Give from API call.
+    let matchID = req.params.match_id || null;
+    let mapNumber = req.params.map_number || null;
+    let mapName = req.query.mapname || null;
+    // Data manipulation inside function.
+    let startTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    let updateStmt = {};
+    let insertStmt = {};
+    let updateSql;
+    let insertSql;
+    // Database calls.
+    let sql =
+      "SELECT * FROM `match` WHERE id = ?";
+    const matchValues = await db.query(sql, matchID);
+
+    if (matchValues[0].end_time !== null && matchValues[0].cancelled !== null)  
+      matchFinalized = false;
+
+    // Throw error if wrong key or finished match.
+    await check_api_key(matchValues[0].api_key, req.params.key, matchFinalized);
+
+    // Begin transaction
+    await withTransaction(db, () => {
+      if(matchValues[0].start_time === null){
+        // Update match stats to have a start time.
+        updateStmt = {
+          start_time: startTime
+        };
+        updateSql = "UPDATE `match` SET ? WHERE id = ?";
+        await db.query(updateSql, [updateStmt, matchID]);
+      }
+      // Get or create mapstats.
+      sql = "SELECT * FROM map_stats WHERE match_id = ? AND map_number = ?";
+      const mapStats = await db.query(sql, [matchID, mapNumber]);
+      if (mapStats.length > 0){
+        updateStmt = {
+          mapnumber: mapNumber,
+          mapname: mapName,
+        };
+        updateSql = "UPDATE map_stats SET ? WHERE match_id = ? AND map_number = ?";
+        // Remove any values that may not be updated.
+        for (let key in updateStmt) {
+          if (updateStmt[key] === null) delete updateStmt[key];
+        }
+        await db.query(updateSql, [updateStmt, matchID, mapNumber]);
+      } else {
+        insertStmt = {
+          match_id: matchID,
+          map_number: mapNumber,
+          map_name: mapName,
+          start_time: startTime,
+          team1_score: 0,
+          team2_score: 0
+        };
+        insertSql = "INSERT INTO map_stats SET ?";
+        await db.query(sql, [insertStmt]);
+      }
+    });
+    res.status(200).send('Success');
   } catch (err) {
-    console.log(err);
     res.status(500).json({ message: err });
   }
 });
 
-/** POST - Route serving to insert a team into the database.
- * @name /create
+/** POST - API Call to finish a match.
+ * @name router.post('/:match_id/map/:map_number/update')
  * @function
  * @memberof module:routes/legacy/api
- * @param {int} req.body[0].user_id - The ID of the user creating the team to claim ownership.
- * @param {string} req.body[0].name - Team name inputted by a user.
- * @param {string} req.body[0].flag - International code for a flag.
- * @param {string} req.body[0].logo - A string representing the logo stored on the webserver.
- * @param {JSON} req.body[0].auth_name - A JSON KV pair containing the SteamID of a player as the key, and a value, or blank string value as the preferred name.
- * @param {string} req.body[0].tag - A string with a shorthand tag for a team.
- * @param {number} req.body[0].public_team - Integer determining if a team is a publically usable team. Either 1 or 0.
- * @see https://steamcommunity.com/sharedfiles/filedetails/?id=719079703
+ * @param {string} req.query.key - The API key given from the game server to compare.
+ * @param {int} req.query.team1_score - The string for which team won the match. team1 or team2.
+ * @param {int} req.query.team2_score - Optional if a team has forfeit a match.
+ * @param {int} req.params.match_id - The given match ID from the path.
+ * @param {int} req.params.map_number - The given map number from the URI path.
  */
+router.post("/:match_id/map/:map_number/update", updateMapRateLimit, async (req, res, next) => {
+  try{
+    // Give from API call.
+    let matchID = req.params.match_id || null;
+    let mapNumber = req.params.map_number || null;
+    let team1Score = req.query.team1_score;
+    let team2Score = req.query.team2_score;
+    // Data manipulation inside function.
+    let updateStmt = {};
+    let updateSql;
+    // Database calls.
+    let sql =
+      "SELECT * FROM `match` WHERE id = ?";
+    const matchValues = await db.query(sql, matchID);
 
-router.post("/create", async (req, res, next) => {
-  let userID = req.body[0].user_id;
-  let teamName = req.body[0].name;
-  let flag = req.body[0].flag;
-  let logo = req.body[0].logo;
-  let auths = req.body[0].auth_name; // Sent into here as a list? Verify somehow?
-  let tag = req.body[0].tag;
-  let public_team = req.body[0].public_team;
-  let teamID = null;
-  newTeam = [
-    {
-      user_id: userID,
-      name: teamName,
-      flag: flag,
-      logo: logo,
-      tag: tag,
-      public_team: public_team
-    }
-  ];
-  let sql =
-    "INSERT INTO team (user_id, name, flag, logo, tag, public_team) VALUES ?";
+    if (matchValues[0].end_time !== null && matchValues[0].cancelled !== null)  
+      matchFinalized = false;
 
-  try {
-    await withTransaction(db, async () => {
-      const insertTeam = await db.query(sql, [
-        newTeam.map(item => [
-          item.user_id,
-          item.name,
-          item.flag,
-          item.logo,
-          item.tag,
-          item.public_team
-        ])
-      ]);
-      teamID = insertTeam.insertId;
-      sql =
-        "INSERT INTO team_auth_names (team_id, auth, name) VALUES (?, ?, ?)";
-      console.log("We made it past the first insert.");
-      for (let key in auths) {
-        await db.query(sql, [teamID, key, auths[key]]);
+    // Throw error if wrong key or finished match.
+    await check_api_key(matchValues[0].api_key, req.params.key, matchFinalized);
+
+    await withTransaction(db, async ()  => {
+      // Get or create mapstats.
+      sql = "SELECT * FROM map_stats WHERE match_id = ? AND map_number = ?";
+      const mapStats = await db.query(sql, [matchID, mapNumber]);
+      if(mapStats.length > 0){
+        if(team1Score !== -1 && team2Score !== -1){
+          updateStmt = {
+            team1_score: team1Score,
+            team2_score: team2Score
+          };
+          updateSql = "UPDATE map_stats SET ? WHERE match_id = ? AND map_number = ?"
+          await db.query(updateSql, [updateStmt, matchID, mapNumber]);
+          res.status(200).send('Success');
+        }
+        else{
+          res.status(400).send('Failed to find map stats object');
+        }
       }
-      res.json({ message: "Team successfully inserted with ID " + teamID });
     });
-  } catch (err) {
+
+  }catch (err){
     res.status(500).json({ message: err });
   }
 });
