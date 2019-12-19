41044465fe71c4790ec5b3d13946a4f139875dea
Evan <etc114@mail.usask.ca>
2019-12-19 17:08:03 -0600

    Rough implementation of forfeit.
    Remove jade dependency.

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error: err.message});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+/** Ensures the user was authenticated through steam OAuth.
+ * @function
+ * @memberof module:routes/mapstats
+ * @function
+ * @inner */
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/auth/steam');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  res.json({message: "Welcome to G5API!"});
 });
 
 module.exports = router;
