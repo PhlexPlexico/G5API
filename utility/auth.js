389a7ee9f0053e67d5eea49be1c8dc86f330c811
Evan <etc114@mail.usask.ca>
2020-04-20 11:52:40 -0600

    Update auth to include admins and super admins.
    Allow new users to be defined as admins if in config.
    
    More user testing.

--- utility/auth.js
+++ utility/auth.js
@@ -19,8 +19,9 @@ passport.deserializeUser((obj, done) => {
 function strategyForEnvironment() {
   let strategy;
   switch(process.env.NODE_ENV) {
-    case 'testing':
+    case 'development':
       strategy = new MockStrategy('steam', returnStrategy);
+      break;
     default:
       strategy = new SteamStrategy({
         returnURL: config.get("Server.hostname")+":"+config.get("Server.port")+'/auth/steam/return',
@@ -35,15 +36,25 @@ function returnStrategy (identifier, profile, done) {
   process.nextTick(async () => {
     profile.identifier = identifier;
     try {
+      let isAdmin = 0;
+      let isSuperAdmin = 0;
+      let superAdminList = config.get("super_admins.steam_ids").split(',');
+      let adminList = config.get("admins.steam_ids").split(',');
       let sql = "SELECT * FROM user WHERE steam_id = ?";
+      // If we are an admin, check!
+      if (superAdminList.indexOf(profile.id) >= 0) {
+        isSuperAdmin = 1;
+      } else if (adminList.indexOf(profile.id) >= 0) {
+        isAdmin = 1;
+      }
       let curUser = await db.query(sql, [profile.id]);
       if (curUser.length < 1) {
         sql = "INSERT INTO user SET ?";
         let newUser = {
           steam_id: profile.id,
           name: profile.displayName,
-          admin: 0,
-          super_admin: 0,
+          admin: isAdmin,
+          super_admin: isSuperAdmin,
           created_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }
         await db.withTransaction(db, async () => {
@@ -51,13 +62,12 @@ function returnStrategy (identifier, profile, done) {
         });
         sql = "SELECT * FROM user WHERE steam_id = ?";
         curUser = await db.query(sql, [profile.id]);
-      }
-    
+      }      
       return done(null, {
         steam_id: profile.id,
         name: profile.displayName,
-        super_admin: curUser[0].super_admin,
-        admin: curUser[0].admin,
+        super_admin: isSuperAdmin,
+        admin: isAdmin,
         id: curUser[0].id
       });
     }
@@ -69,47 +79,5 @@ function returnStrategy (identifier, profile, done) {
 }
 
 passport.use(strategyForEnvironment());
-
-// passport.use(new SteamStrategy({
-//     returnURL: config.get("Server.hostname")+":"+config.get("Server.port")+'/auth/steam/return',
-//     realm: config.get("Server.hostname")+":"+config.get("Server.port"),
-//     apiKey: config.get("Server.steamAPIKey"),
-//   },
-//   (identifier, profile, done) => {
-//     process.nextTick(async () => {
-//       profile.identifier = identifier;
-//       try {
-//         let sql = "SELECT * FROM user WHERE steam_id = ?";
-//         let curUser = await db.query(sql, [profile.id]);
-//         if (curUser.length < 1) {
-//           sql = "INSERT INTO user SET ?";
-//           let newUser = {
-//             steam_id: profile.id,
-//             name: profile.displayName,
-//             admin: 0,
-//             super_admin: 0,
-//             created_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
-//           }
-//           await db.withTransaction(db, async () => {
-//             curUser = await db.query(sql, [newUser]);
-//           });
-//         }
-      
-//         return done(null, {
-//           steam_id: profile.id,
-//           name: profile.displayName,
-//           super_admin: curUser[0].super_admin,
-//           admin: curUser[0].admin,
-//           id: curUser[0].id
-//         });
-//       }
-//       catch ( err ) {
-//         console.log("ERRORERRORERRORERRORERRORERRORERRORERROR " + err + "ERRORERRORERRORERRORERRORERRORERRORERROR");
-//         return done(null, null);
-//       }
-//     });
-//   }
-// ));
-
 
 module.exports = passport;
