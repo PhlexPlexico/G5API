e40d6b8d278db61e6bff633d782fbbc4a9ab7356
PhlexPlexico <flexing@phlexplexi.co>
2020-10-13 15:09:48 -0600

    Updated if we have null teams in the match table.
    Now allow team deletion even if a team has data in the database.

--- routes/teams.js
+++ routes/teams.js
@@ -508,32 +508,36 @@ router.delete("/", Utils.ensureAuthenticated, async (req, res) => {
   } else {
     try {
       // First find any matches/mapstats/playerstats associated with the team.
-      let playerStatSql =
-        "SELECT COUNT(*) as RECORDS FROM player_stats WHERE team_id = ?";
-      let mapStatSql =
-        "SELECT COUNT(*) as RECORDS FROM map_stats WHERE winner = ?";
-      let matchSql =
-        "SELECT COUNT(*) as RECORDS FROM `match` WHERE team1_id = ? OR team2_id = ?";
-      const playerStatCount = await db.query(playerStatSql, teamID);
-      const mapStatCount = await db.query(mapStatSql, teamID);
-      const matchCount = await db.query(matchSql, [teamID, teamID]);
-      if (
-        playerStatCount[0].RECORDS > 0 ||
-        mapStatCount[0].RECORDS > 0 ||
-        matchCount[0].RECORDS > 0
-      ) {
-        throw (
-          "Cannot delete team as it has more than one of the following true:\n" +
-          "Player Stat Records: " +
-          playerStatCount[0].RECORDS +
-          "\n" +
-          "Map Stat Records: " +
-          mapStatCount[0].RECORDS +
-          "\n" +
-          "Match Records: " +
-          matchCount[0].RECORDS
-        );
-      }
+      // TODO:Technically, since the database can live without TeamIDs, we can
+      //      now delete as needed and deal with how the data is represented on the front end.
+      //      So, we remove the team_auths, and the team from the platform, but the names are
+      //      kept in the Match strings to show some historical data.
+      // let playerStatSql =
+      //   "SELECT COUNT(*) as RECORDS FROM player_stats WHERE team_id = ?";
+      // let mapStatSql =
+      //   "SELECT COUNT(*) as RECORDS FROM map_stats WHERE winner = ?";
+      // let matchSql =
+      //   "SELECT COUNT(*) as RECORDS FROM `match` WHERE team1_id = ? OR team2_id = ?";
+      // const playerStatCount = await db.query(playerStatSql, teamID);
+      // const mapStatCount = await db.query(mapStatSql, teamID);
+      // const matchCount = await db.query(matchSql, [teamID, teamID]);
+      // if (
+      //   playerStatCount[0].RECORDS > 0 ||
+      //   mapStatCount[0].RECORDS > 0 ||
+      //   matchCount[0].RECORDS > 0
+      // ) {
+      //   throw (
+      //     "Cannot delete team as it has more than one of the following true:\n" +
+      //     "Player Stat Records: " +
+      //     playerStatCount[0].RECORDS +
+      //     "\n" +
+      //     "Map Stat Records: " +
+      //     mapStatCount[0].RECORDS +
+      //     "\n" +
+      //     "Match Records: " +
+      //     matchCount[0].RECORDS
+      //   );
+      // }
       // Otherwise, let's continue with delete. Start with auths.
       await db.withTransaction(async () => {
         let deleteTeamAuthSql = "DELETE FROM team_auth_names WHERE team_id = ?";
@@ -642,6 +646,7 @@ router.get("/:team_id/result/:match_id", async (req, res) => {
     let matchSql = "SELECT * FROM `match` WHERE id = ?";
     let teamSql = "SELECT * FROM team WHERE id = ?";
     let statusString = "";
+    let otherName = "";
     const curMatch = await db.query(matchSql, [matchId]);
     if (curMatch.length === 0) {
       res.status(404).json({ result: "Team did not participate in match." });
@@ -671,25 +676,26 @@ router.get("/:team_id/result/:match_id", async (req, res) => {
         }
       }
     }
-    // Start building the return string.
+    if (otherTeam.length == 0) otherName = "Team Removed From Match";
+    else otherName = otherTeam[0].name;
     if (
       curMatch[0].end_time == null &&
       (curMatch[0].cancelled == false || curMatch[0].cancelled == null) &&
       curMatch[0].start_time != null
     )
       statusString =
-        "Live, " + myScore + ":" + otherTeamScore + " vs " + otherTeam[0].name;
+        "Live, " + myScore + ":" + otherTeamScore + " vs " + otherName;
     else if (myScore < otherTeamScore)
       statusString =
-        "Lost, " + myScore + ":" + otherTeamScore + " vs " + otherTeam[0].name;
+        "Lost, " + myScore + ":" + otherTeamScore + " vs " + otherName;
     else if (myScore > otherTeamScore)
       statusString =
-        "Won, " + myScore + ":" + otherTeamScore + " vs " + otherTeam[0].name;
+        "Won, " + myScore + ":" + otherTeamScore + " vs " + otherName;
     else if (curMatch[0].winner != null)
-      statusString = "Forfeit win vs " + otherTeam[0].name;
+      statusString = "Forfeit win vs " + otherName;
     else
       statusString =
-        "Tied, " + myScore + ":" + otherTeamScore + " vs " + otherTeam[0].name;
+        "Tied, " + myScore + ":" + otherTeamScore + " vs " + otherName;
     res.json({ result: statusString });
   } catch (err) {
     res.status(500).json({ message: err.toString() });
