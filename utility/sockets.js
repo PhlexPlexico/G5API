9c358ed954bd86e1328245372e0ba40419bea396
phlexplexico <flexing@phlexplexi.co>
2022-08-29 15:25:07 -0600

    Remove unused socket class.

--- utility/sockets.js
+++
@@ -1,9 +0,0 @@
-import { Server } from "socket.io";
-
-const socketAPI = new Server();
-
-socketAPI.on( "connection", function( socket ) {
-    console.log( "A user connected" );
-});
-
-export default socketAPI;
